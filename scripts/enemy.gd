25e86a64d04de0b51ccde66d47de932b9bd9be59
Szymon Piekut <szymonpiekut054@gmail.com>
2025-12-12 11:25:35 +0100

    added randomised speed to enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,17 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var t
-func test():
-	print("test")
+var speed = snapped(RandomNumberGenerator.new().randf_range(0.01, 0.033), 0.0001)
+
+
+	
 	
 func _physics_process(delta: float) -> void:
-	t = delta * 0.4
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	$".".position = $".".position.lerp(Global.test, 0.033)
+	$".".position = $".".position.lerp(Global.test, speed)
 		
 
 	move_and_slide()
--- scripts/main.gd
+++ scripts/main.gd
@@ -5,4 +5,5 @@ func _process(delta: float) -> void:
 	var enemy_movement_vector : Vector2 = Vector2($"player".position.x,$"enemy".position.y)
 	Global.test = enemy_movement_vector
 	
-		
+	print("en_1: ", snapped($"enemy".speed, 0.0001) ," en_2: ", $"enemy2".speed)
+	
